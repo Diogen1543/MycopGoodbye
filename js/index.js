4146e7b8eeee690dee019912507ee05f5a6cdb52
Anastasiya Zvezdina <zvezdinaan@gmail.com>
2020-09-17 11:14:49 +0500

    доработан слайдер, отображение элементов скрытых

--- js/index.js
+++ js/index.js
@@ -1,27 +1,18 @@
 const navButton = document.querySelector('.header__nav-mini');
 const nav = document.querySelector('.header__nav-links');
 const workItem = document.querySelector('.work__item');
-const workHover = document.querySelector('.work__hover');
-
 
 navButton.addEventListener('click', ()=>{
     event.preventDefault();
     nav.classList.toggle('header__nav-links_mobile');
     navButton.classList.toggle('header__nav-mini_active');
-})
-
-
-function hoverWorkItem(item){
-     item.classList.toggle('work__hover_active')
-}
-workItem.addEventListener('mouseover', hoverWorkItem(workHover));
-
+});
 
 /* Slider */
 $(function() {
 
    $("[slider]").slick({
-       // dots: true,
+        dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
